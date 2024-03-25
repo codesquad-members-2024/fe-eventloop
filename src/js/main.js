fe51263457aefa6d5d48c748f5bff0683a13408b
melroh629 <jw.roh629@gmail.com>
2024-03-25 18:29:40 +0900

    chore: acorn, acron-walk 설치 밎 vite config변경

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,7 @@
-console.log(1);
+import { parse } from '/node_modules/.vite/deps/acorn.js?v=ca8a5aa0';
+
+import * as acornWalk from '/node_modules/.vite/deps/acorn-walk.js?v=ca8a5aa0';
+
+const code = `const a = 1;`;
+const ast = parse(code, { ecmaVersion: 2020 }); // 'parse' 함수를 사용합니다.
+console.log(ast);
