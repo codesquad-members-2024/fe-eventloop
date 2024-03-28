fa790b2db014ea3982aad86fa3754bf83dbf2413
minjeongHEO <hmjoung9770@gmail.com>
2024-03-28 16:48:56 +0900

    refactor: 콜백을 함수로 분리 및 색상 랜덤 지정하기

--- src/js/EventLoopHandler.js
+++ src/js/EventLoopHandler.js
@@ -1,82 +1,106 @@
 export default class EventLoopHandler {
   constructor(callBacks, classNames) {
+    this.callBacks = callBacks;
     this.callStackClassName = classNames.callStackClassName;
     this.webApiClassName = classNames.webApiClassName;
     this.microQClassName = classNames.microQClassName;
     this.macroQClassName = classNames.macroQClassName;
-    this.callBacks = callBacks;
     this.EventLoopControl();
   }
 
   // 애니메이션
   runAnimation() {}
 
+  removeMatchingElement(callBack, className) {
+    const callStackTarget = document.querySelector(className);
+    const stuffs = Array.from(callStackTarget.children);
+    stuffs.forEach((stuff) => {
+      if (stuff.textContent === callBack.callBackCode) {
+        stuff.remove();
+      }
+    });
+  }
+
   // 태그 지우기
   async removeElementByTextAsync(callBack, className) {
     return new Promise((resolve) => {
       setTimeout(() => {
-        const callStackTarget = document.querySelector(className);
-        const stuffs = Array.from(callStackTarget.children);
-        stuffs.forEach((stuff) => {
-          if (stuff.textContent === callBack.callBackCode) {
-            stuff.remove();
-          }
-        });
+        this.removeMatchingElement(callBack, className);
         resolve(true);
-      }, 5000);
+      }, 2000);
     });
   }
 
+  // 랜덤 색상
+  generateRandomColor() {
+    // 0x1000000 (16777216)보다 작은 랜덤 정수를 생성하고, 16진수 문자열로 변환
+    const randomColor = Math.floor(Math.random() * 0x1000000).toString(16);
+    // 색상 코드가 항상 6자리가 되도록 앞에 0을 붙임
+    return `#${'0'.repeat(6 - randomColor.length) + randomColor}`;
+  }
+
   // 태그 생성
-  createAnimationDivMarkup(callBack) {
-    const newHTML = `<div class="animation__stuff">${callBack.callBackCode}</div>`;
+  createAnimationDivMarkup(callBack, backgroundColor) {
+    const newHTML = `<div class="animation__stuff" style="background-color: ${backgroundColor};">${callBack.callBackCode}</div>`;
     return newHTML;
   }
 
   // 태그 생성
-  appendTag(callBack, className) {
+  appendTag(callBack, className, idx) {
     const Target2append = document.querySelector(className);
-    const animationDivHtml = this.createAnimationDivMarkup(callBack);
+    let backgroundColor = null;
+
+    // if (this.bgColor.hasOwnProperty(typeName)) {
+    // 위의 경우는 hasOwnProperty 메서드가 오버라이드되었거나, this.bgColor 객체가 null 또는 undefined인 경우에는 오류가 발생할 수 있습니다.
+    const isExistence = Object.prototype.hasOwnProperty.call(this.callBacks[idx], 'bgColor');
+
+    if (isExistence) {
+      backgroundColor = this.callBacks[idx].bgColor;
+    } else {
+      backgroundColor = this.generateRandomColor();
+      this.callBacks[idx].bgColor = backgroundColor;
+    }
+    const animationDivHtml = this.createAnimationDivMarkup(callBack, backgroundColor);
     Target2append.insertAdjacentHTML('beforeend', animationDivHtml);
   }
 
   /** Promise.then, Promise.catch, Promise.finally, MutationObserver 콜백 */
-  async setMicroTaskQueue(callBack) {
+  async setMicroTaskQueue(callBack, idx) {
     //마이크로 태스크 큐의 작업은 매크로 태스크보다 우선적으로 처리된다.
     const isElementRemoved = await this.removeElementByTextAsync(callBack, this.webApiClassName);
-    if (isElementRemoved) this.appendTag(callBack, this.microQClassName);
+    if (isElementRemoved) this.appendTag(callBack, this.microQClassName, idx);
   }
 
   /** setTimeout, setInterval, setImmediate, I/O 작업과 같은 비동기 작업의 콜백 */
-  setMacroTaskQueue(callBack) {
+  setMacroTaskQueue(callBack, idx) {
     setTimeout(async () => {
       const isElementRemoved = await this.removeElementByTextAsync(callBack, this.webApiClassName);
-      if (isElementRemoved) this.appendTag(callBack, this.macroQClassName);
+      if (isElementRemoved) this.appendTag(callBack, this.macroQClassName, idx);
     }, callBack.delay);
   }
 
-  classifyTaskQueues(callBack) {
-    if (callBack.type === 'then') this.setMicroTaskQueue(callBack);
-    if (callBack.type === 'catch') this.setMicroTaskQueue(callBack);
-    if (callBack.type === 'setTimeout') this.setMacroTaskQueue(callBack);
+  classifyTaskQueues(callBack, idx) {
+    if (callBack.type === 'then') this.setMicroTaskQueue(callBack, idx);
+    if (callBack.type === 'catch') this.setMicroTaskQueue(callBack, idx);
+    if (callBack.type === 'setTimeout') this.setMacroTaskQueue(callBack, idx);
   }
 
-  async runEvent(callBack) {
+  async runEvent(callBack, idx) {
     //CALL STACK
-    this.appendTag(callBack, this.callStackClassName);
+    this.appendTag(callBack, this.callStackClassName, idx);
     const isElementRemoved = await this.removeElementByTextAsync(callBack, this.callStackClassName);
 
     //WEB APIS
-    if (isElementRemoved) this.appendTag(callBack, this.webApiClassName);
+    if (isElementRemoved) this.appendTag(callBack, this.webApiClassName, idx);
 
     //MICRO & MACRO TASK QUEUE
-    this.classifyTaskQueues(callBack);
+    this.classifyTaskQueues(callBack, idx);
   }
 
   // 콜스택 컨트롤
   async EventLoopControl() {
-    for (const callback of this.callBacks) {
-      await this.runEvent(callback);
+    for (const [idx, callback] of this.callBacks.entries()) {
+      await this.runEvent(callback, idx);
     }
     // this.callBacks.forEach((callBack) => {
     // });
