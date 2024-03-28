21c77d2a89ef16de83ea63e297ff07108ee4de15
dmdeh <131264106+dmdeh@users.noreply.github.com>
2024-03-28 11:25:12 +0900

    style: style 수정 및 eventloop img animation 추가

--- src/js/codeAnalyze.js
+++ src/js/codeAnalyze.js
@@ -5,15 +5,12 @@ const macroTasks = ["setTimeout", "setInterval"];
 
 function getCalleeName(node) {
   const callee = node.callee;
-  if (callee?.type === "Identifier") {
-    // 앞이 undefined나 null이면 undefined
+  if (callee?.type === "Identifier") { // 앞이 undefined나 null이면 undefined
     return callee.name;
   } else if (callee?.property?.type === "Identifier") {
     return callee.property.name;
   } else if (
-    callee?.object?.type === "Identifier" &&
-    callee?.property?.type === "Identifier"
-  ) {
+    callee?.object?.type === "Identifier" && callee?.property?.type === "Identifier") {
     return callee.object.name + "." + callee.property.name;
   } else {
     return null;
