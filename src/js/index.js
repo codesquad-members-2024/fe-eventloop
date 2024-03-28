773792b08af31e1ecd3f0bb20c9c76de6ac9a3f9
minjeongHEO <hmjoung9770@gmail.com>
2024-03-28 15:02:22 +0900

    refactor: class명들을 파라미터로 받기

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,13 @@ import SubmitHandler from './SubmitHandler.js';
 
 const formId = 'code-form';
 const textId = 'code-input';
+const classNames = {
+  callStackClassName: '.animation__call_stack_box',
+  webApiClassName: '.animation__web_api_box',
+  microQClassName: '.animation__micro_task_box',
+  macroQClassName: '.animation__macro_task_box',
+};
 
 window.onload = () => {
-  new SubmitHandler(formId, textId);
+  new SubmitHandler(formId, textId, classNames);
 };
