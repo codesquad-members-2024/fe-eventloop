f77d3f33fea0c352cfc8da0891e2c461346d6cd5
melroh629 <jw.roh629@gmail.com>
2024-03-28 21:57:47 +0900

    feat: acorn과 acorn-walk를 사용하여 데이터 가공

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,6 @@
-import { parse } from '/node_modules/.vite/deps/acorn.js?v=ca8a5aa0';
+// import { CallStack, WebAPI, TaskQueue, EventLoop } from './taskModel.js';
+import { parseCode } from './parsing.js';
+import { code } from './test-code.js';
 
-import * as acornWalk from '/node_modules/.vite/deps/acorn-walk.js?v=ca8a5aa0';
-
-const code = `const a = 1;`;
-const ast = parse(code, { ecmaVersion: 2020 }); // 'parse' 함수를 사용합니다.
-console.log(ast);
+const test = parseCode(code);
+console.log(test);
