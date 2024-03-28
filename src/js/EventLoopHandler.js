cd7fe6c2ef2b0dba7b2d99b12e8126a2aaae24a7
minjeongHEO <hmjoung9770@gmail.com>
2024-03-28 11:26:23 +0900

    WIP: web api 박스까지 생성 해주는 중

--- src/js/EventLoopHandler.js
+++ src/js/EventLoopHandler.js
@@ -2,6 +2,8 @@ export default class EventLoopHandler {
   constructor(callBacks) {
     this.callStackClassName = '.animation__call_stack_box';
     this.webApiClassName = '.animation__web_api_box';
+    this.microQClassName = '.animation__micro_task_box';
+    this.macroQClassName = '.animation__macro_task_box';
     this.callBacks = callBacks;
     this.EventLoopControl();
   }
@@ -40,21 +42,42 @@ export default class EventLoopHandler {
     // console.log('-------------생성--------------');
   }
 
-  classifyTaskQueues() {}
+  /** Promise.then, Promise.catch, Promise.finally, MutationObserver 콜백 */
+  async setMicroTaskQueue(callBack) {
+    //마이크로 태스크 큐의 작업은 매크로 태스크보다 우선적으로 처리된다.
+    const webApiRemove = await this.removeHTML(callBack, this.webApiClassName);
+    if (webApiRemove) this.appendTag(callBack, this.microQClassName);
+  }
+
+  /** setTimeout, setInterval, setImmediate, I/O 작업과 같은 비동기 작업의 콜백 */
+  setMacroTaskQueue(callBack) {
+    setTimeout(async () => {
+      const webApiRemove = await this.removeHTML(callBack, this.webApiClassName);
+      if (webApiRemove) this.appendTag(callBack, this.macroQClassName);
+    }, callBack.delay);
+  }
+
+  classifyTaskQueues(callBack) {
+    if (callBack.type === 'then') setMicroTaskQueue(callBack);
+    if (callBack.type === 'catch') setMicroTaskQueue(callBack);
+    if (callBack.type === 'setTimeout') setMacroTaskQueue(callBack);
+  }
 
   async runEvent(callBack) {
+    //call stack에 추가, 삭제
     this.appendTag(callBack, this.callStackClassName);
-    // this.runAnimation()
     const callBackRemove = await this.removeHTML(callBack, this.callStackClassName);
 
+    //web api에 추가 (삭제 시 settimeout은 별도 처리 필요)
     if (callBackRemove) this.appendTag(callBack, this.webApiClassName);
-    const webApiRemove = await this.removeHTML(callBack, this.webApiClassName);
+    // const webApiRemove = await this.removeHTML(callBack, this.webApiClassName);
 
-    if (webApiRemove) this.classifyTaskQueues(callBack);
+    this.classifyTaskQueues(callBack);
   }
 
   // 콜스택 컨트롤
   async EventLoopControl() {
+    // console.log(this.callBacks);
     for (const callback of this.callBacks) {
       await this.runEvent(callback);
     }
@@ -69,4 +92,14 @@ export default class EventLoopHandler {
  * 3. web API로 우측으로 이동해 -> 바로 생겨 -> 사라져
  * 4. micro/macro task queue중 어디로 갈지 파악하고 이동해
  * 5.
+ * 
+ * Task Queue 
+    : 일반적으로 '매크로 태스크 큐'라고도 불린다.
+      setTimeout, setInterval, setImmediate, I/O 작업과 같은 비동기 작업의 콜백이 위치하는 큐다.
+      이벤트 루프가 Task Queue의 작업을 하나씩 콜 스택으로 이동시켜 처리한다.
+    
+    Microtask Queue
+    : Promise.then, Promise.catch, Promise.finally, MutationObserver 콜백이 위치하는 큐다.
+      마이크로 태스크 큐의 작업은 매크로 태스크보다 우선적으로 처리된다.
+ * 
  */
