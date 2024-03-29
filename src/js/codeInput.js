150bcde6525b1cc86612792ecb183413b1423ffb
정채영 <chaeyoung@jeongchaeyeong-ui-MacBookAir.local>
2024-03-29 16:15:37 +0900

    refactor: request api 다른 파일로 분리

--- src/js/codeInput.js
+++ src/js/codeInput.js
@@ -1,3 +1,5 @@
+import { postData } from "./requestApi.js";
+
 export function getCodeInput() {
   const runButton = document.querySelector(".submit-btn");
 
@@ -5,23 +7,4 @@ export function getCodeInput() {
     const code = document.getElementById("codeInput").value;
     postData("codeInput", code);
   });
-}
-
-function postData(path, data) {
-  fetch(`http://localhost:3000/${path}`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ code: data }),
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Failed to send code");
-      }
-      console.log("Code sent successfully");
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
 }
