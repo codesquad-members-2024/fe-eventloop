599ee3a53310891b9656bf535fda04d2d97d4a84
minjeongHEO <hmjoung9770@gmail.com>
2024-03-29 14:25:31 +0900

    WIP: 스택에 있는 태그 옮기기(이벤트 루프) - 해당하는 각 요소 각각 constructor에 생성 - 이벤트 루프 함수 생성

--- src/js/EventLoopHandler.js
+++ src/js/EventLoopHandler.js
@@ -1,16 +1,36 @@
-export default class EventLoopHandler {
+class EventLoopHandler {
   constructor(callBacks, classNames) {
     this.callBacks = callBacks;
     this.callStackClassName = classNames.callStackClassName;
     this.webApiClassName = classNames.webApiClassName;
     this.microQClassName = classNames.microQClassName;
     this.macroQClassName = classNames.macroQClassName;
+    this.statusOfcallStack = [];
+    this.statusOfWebApi = [];
+    this.statusOfMicroQ = [];
+    this.statusOfMacroQ = [];
     this.EventLoopControl();
   }
 
   // 애니메이션
   runAnimation() {}
 
+  /**
+   * macro Q가 비어있는지 확인
+   * @returns true - macro Q가 비어있음
+   * @returns false
+   */
+  checkIfMacroQIsEmpty() {
+    return document.querySelector(this.macroQClassName).children.length === 0 ? true : false;
+  }
+  /**
+   * micro Q가 비어있는지 확인
+   * @returns true - micro Q가 비어있음
+   * @returns false
+   */
+  checkIfMicroQIsEmpty() {
+    return document.querySelector(this.microQClassName).children.length === 0 ? true : false;
+  }
   /**
    * 콜스택이 비어있는지 확인
    * @returns true - 콜스택이 비어있음
@@ -35,6 +55,25 @@ export default class EventLoopHandler {
     return new Promise((resolve) => {
       setTimeout(() => {
         this.removeMatchingElement(callBack, className);
+        this.updateStatusByClassName(callBack, className, 'pop');
+        resolve(true);
+      }, 2000);
+    });
+  }
+
+  removeFirstElement(className) {
+    const ElementTarget = document.querySelector(className);
+    if (ElementTarget.children.length > 0) {
+      ElementTarget.children[0].remove(); // 첫 번째 자식 요소 제거
+    }
+  }
+
+  // 첫번째 태그 지우기
+  async removeFirstElementByTextAsync(callBack, className) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        this.removeFirstElement(callBack, className);
+        this.updateStatusByClassName(callBack, className, 'pop');
         resolve(true);
       }, 2000);
     });
@@ -58,7 +97,6 @@ export default class EventLoopHandler {
     // if (this.bgColor.hasOwnProperty(typeName)) {
     // 위의 경우는 hasOwnProperty 메서드가 오버라이드되었거나, this.bgColor 객체가 null 또는 undefined인 경우에는 오류가 발생할 수 있습니다.
     const isExistence = Object.prototype.hasOwnProperty.call(this.callBacks[idx], 'bgColor');
-
     if (isExistence) return this.callBacks[idx].bgColor;
 
     const backgroundColor = this.generateRandomColor();
@@ -66,13 +104,37 @@ export default class EventLoopHandler {
     return backgroundColor;
   }
 
+  getCallBackStatusByClassName(className) {
+    if (className === this.callStackClassName) return this.statusOfcallStack[this.statusOfcallStack.length - 1];
+    if (className === this.webApiClassName) return this.statusOfWebApi[this.statusOfWebApi.length - 1];
+    if (className === this.microQClassName) return this.statusOfMicroQ[this.statusOfMicroQ.length - 1];
+    if (className === this.macroQClassName) return this.statusOfMacroQ[this.statusOfMacroQ.length - 1];
+  }
+
+  updateStatusByClassName(callBack, className, type) {
+    if (type === 'push') {
+      if (className === this.callStackClassName) this.statusOfcallStack.push(callBack);
+      if (className === this.webApiClassName) this.statusOfWebApi.push(callBack);
+      if (className === this.microQClassName) this.statusOfMicroQ.push(callBack);
+      if (className === this.macroQClassName) this.statusOfMacroQ.push(callBack);
+    }
+    if (type === 'pop') {
+      if (className === this.callStackClassName) this.statusOfcallStack.pop(callBack);
+      if (className === this.webApiClassName) this.statusOfWebApi.pop(callBack);
+      if (className === this.microQClassName) this.statusOfMicroQ.pop(callBack);
+      if (className === this.macroQClassName) this.statusOfMacroQ.pop(callBack);
+    }
+  }
+
   // 태그 생성
   appendTag(callBack, className, idx) {
     const Target2append = document.querySelector(className);
-
-    let backgroundColor = this.specifyBackgroundColor(idx);
+    let backgroundColor = '#fffff';
+    if (idx) backgroundColor = this.specifyBackgroundColor(idx);
+    if (callBack.bgColor) backgroundColor = callBack.bgColor;
     const animationDivHtml = this.createAnimationDivMarkup(callBack, backgroundColor);
     Target2append.insertAdjacentHTML('beforeend', animationDivHtml);
+    this.updateStatusByClassName(callBack, className, 'push');
   }
 
   /** Promise.then, Promise.catch, Promise.finally, MutationObserver 콜백 */
@@ -96,28 +158,55 @@ export default class EventLoopHandler {
     if (callBack.type === 'setTimeout') this.setMacroTaskQueue(callBack, idx);
   }
 
-  async runEvent(callBack, idx) {
-    //CALL STACK
-    this.appendTag(callBack, this.callStackClassName, idx);
-    const isElementRemoved = await this.removeElementByTextAsync(callBack, this.callStackClassName);
-
-    //WEB APIS
-    if (isElementRemoved) this.appendTag(callBack, this.webApiClassName, idx);
-
-    //MICRO & MACRO TASK QUEUE
-    this.classifyTaskQueues(callBack, idx);
-    //CALL STACK
+  async runEventLoop() {
     const IsEmpty = this.checkIfCallStackIsEmpty();
-    // if(IsEmpty) setMicroTaskQueue 다 빼내고, setMacroTaskQueue를 뺀다.\
+
+    if (IsEmpty) {
+      const isMicroQIsEmpty = this.checkIfMicroQIsEmpty();
+      const isMacroQIsEmpty = this.checkIfMacroQIsEmpty();
+      if (isMicroQIsEmpty && isMacroQIsEmpty) return true;
+
+      if (!isMicroQIsEmpty) {
+        const microCallBackObj = this.getCallBackStatusByClassName(this.microQClassName);
+        await this.removeFirstElementByTextAsync(this.microQClassName);
+        this.appendTag(microCallBackObj, this.callStackClassName);
+        return await this.removeElementByTextAsync(microCallBackObj, this.callStackClassName);
+      }
+      if (isMicroQIsEmpty && !isMacroQIsEmpty) {
+        const macroCallBackObj = this.getCallBackStatusByClassName(this.macroQClassName);
+        await this.removeFirstElementByTextAsync(this.macroQClassName);
+        this.appendTag(macroCallBackObj, this.callStackClassName);
+        return await this.removeElementByTextAsync(macroCallBackObj, this.callStackClassName);
+      }
+    }
+  }
+
+  async runEvent(callBack, idx) {
+    //EVENT LOOP
+    const isEventLoopSuccess = idx === 0 ? true : await this.runEventLoop();
+
+    if (isEventLoopSuccess) {
+      //CALL STACK
+      this.appendTag(callBack, this.callStackClassName, idx);
+      const isElementRemoved = await this.removeElementByTextAsync(callBack, this.callStackClassName);
+
+      //WEB APIS
+      if (isElementRemoved) this.appendTag(callBack, this.webApiClassName, idx);
+
+      //MICRO & MACRO TASK QUEUE
+      this.classifyTaskQueues(callBack, idx);
+
+      //CALL STACK
+      //EVENT LOOP
+    }
   }
 
   // 콜스택 컨트롤
   async EventLoopControl() {
+    console.log(this.callBacks);
     for (const [idx, callback] of this.callBacks.entries()) {
       await this.runEvent(callback, idx);
     }
-    // this.callBacks.forEach((callBack) => {
-    // });
   }
 }
 /**
@@ -137,3 +226,5 @@ export default class EventLoopHandler {
       마이크로 태스크 큐의 작업은 매크로 태스크보다 우선적으로 처리된다.
  * 
  */
+
+export default EventLoopHandler;
