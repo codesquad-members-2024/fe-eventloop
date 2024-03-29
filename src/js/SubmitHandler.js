e1f95a6d4a0ad8ecf151318080a32314299cb81f
minjeongHEO <hmjoung9770@gmail.com>
2024-03-29 14:26:49 +0900

    refactor: export 맨 밑에 지정

--- src/js/SubmitHandler.js
+++ src/js/SubmitHandler.js
@@ -1,6 +1,6 @@
 import EventLoopHandler from './EventLoopHandler.js';
 
-export default class SubmitHandler {
+class SubmitHandler {
   constructor(formId, textId, classNames) {
     this.formId = formId;
     this.textId = textId;
@@ -74,7 +74,6 @@ export default class SubmitHandler {
 
     // 재귀적으로 자식 노드 순회
     Object.keys(parseNode).forEach((key) => {
-      //*1)
       if (parseNode.hasOwnProperty(key)) {
         const child = parseNode[key];
         if (typeof child === 'object' && child !== null) {
@@ -110,14 +109,6 @@ export default class SubmitHandler {
 }
 
 /**
- * *1)
- * 객체에 해당 key로 명시된 속성이 직접적으로 정의되어 있는지를 확인하기 위함입니다.
- * 이 방법은 속성이 존재하지 않는 것과 속성의 값이 "falsy"인 것(예: false, 0, "", null, undefined, NaN) 사이의 차이를 명확하게 구분합니다.
- * 속성의 값과 관계없이, 속성의 존재 자체만을 확인합니다. 따라서, 해당 속성이 "falsy" 값을 가지고 있더라도, 속성이 객체에 직접 정의되어 있다면 true를 반환합니다.
- *
- * if (parseNode[key])와 같은 조건문은 속성의 값이 "truthy"인지 여부를 검사하므로, 속성의 존재 여부와는 관계없이 속성의 값에 따라 조건문이 실행됩니다.
- * 속성의 존재 여부만을 명확히 확인하고자 할 때는 hasOwnProperty를 사용하는 것이 적합합니다.
- * 
  *  < 필요한 정보 >
      fetch
         : CallExpression 노드에서 callee가 fetch인 경우
@@ -126,3 +117,5 @@ export default class SubmitHandler {
      setTimeout의 콜백 함수
         : CallExpression 노드에서 callee가 setTimeout인 경우를 찾고, arguments의 두 번째 요소(콜백 함수)를 추출
  */
+
+export default SubmitHandler;
