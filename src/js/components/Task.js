8a9d6544c762ac40cf1b1db4f9df9364014c4ab4
mi-hye <satge13@gmail.com>
2024-04-02 20:23:39 +0900

    refactor: Move Task.js to component

---
+++ src/js/components/Task.js
@@ -0,0 +1,21 @@
+import { TASK_DELAY } from "../utils/constans.js";
+
+class Task {
+	constructor(className) {
+		this.className = className;
+		this.element = document.querySelector(`.${className}`);
+	}
+
+	removeTask() {
+		this.element.innerHTML = "";
+	}
+
+	appendTask(calleeName) {
+		this.element.innerHTML = `<div class="task">${calleeName}</div>`;
+		setTimeout(() => {
+			this.removeTask();
+		}, TASK_DELAY);
+	}
+}
+
+export default Task;
