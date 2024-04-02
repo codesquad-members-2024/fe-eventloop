e2574b45df32ba40565e7e117f9dee896b38326e
dmdeh <131264106+dmdeh@users.noreply.github.com>
2024-04-02 17:08:54 +0900

    refactor: AnimationGenerator 리팩토링

--- src/component/animationHelper/animationGenerator.js
+++ src/component/animationHelper/animationGenerator.js
@@ -56,76 +56,49 @@ export class AnimationGenerator{
         element.remove()
     }
 
+    animateElement(elementId, prevLocation, newLocation) {
+        const element = document.getElementById(elementId);
+        return element.animate(
+            [
+                {
+                    transform: `translate(${prevLocation.x}px, ${prevLocation.y}px)`,
+                },
+                {
+                    transform: `translate(${newLocation.x}px, ${newLocation.y}px)`,
+                },
+            ],
+            {
+                duration: 2000,
+                easing: "ease-in-out",
+                direction: "normal",
+                fill: "forwards",
+            }
+        );
+    }
+    
     applyWepApiInAnimation() {
         const prevLocation = locationCalculater.getLocation("call-stack-container");
-        const Location = locationCalculater.getLocation(this.drawPosition);
-        const element = document.getElementById(`${this.id}`);
-        const animation = element.animate(
-            [
-                {
-                    transform: `translate(${prevLocation.x}px, ${prevLocation.y}px)`,
-                },
-                {
-                    transform: `translate(${Location.x}px, ${Location.y}px)`,
-                },
-            ],
-            {
-                duration: 2000,
-                easing: "ease-in-out",
-                direction: "normal",
-                fill: "forwards",
-            }
-        );
+        const newLocation = locationCalculater.getLocation(this.drawPosition);
+        this.animateElement(this.id, prevLocation, newLocation);
     }
-
+    
     applyQueueInAnimation() {
         const prevLocation = locationCalculater.getLocation("wep-Api-container");
-        const Location = locationCalculater.getQueueLocation(this.drawPosition);
-        const element = document.getElementById(`${this.id}`);
-        const animation = element.animate(
-            [
-                {
-                    transform: `translate(${prevLocation.x}px, ${prevLocation.y}px)`,
-                },
-                {
-                    transform: `translate(${Location.x}px, ${Location.y}px)`,
-                },
-            ],
-            {
-                duration: 2000,
-                easing: "ease-in-out",
-                direction: "normal",
-                fill: "forwards",
-            }
-        );
+        const newLocation = locationCalculater.getQueueLocation(this.drawPosition);
+        this.animateElement(this.id, prevLocation, newLocation);
     }
-
+    
     async applyQueueoutAnimation(queueContainer) {
         const prevLocation = locationCalculater.getQueueLocation(queueContainer);
-        const Location = locationCalculater.getLocation(this.drawPosition);
-        const element = document.getElementById(`${this.id}`);
-        const animation = element.animate(
-            [
-                {
-                    transform: `translate(${prevLocation.x}px, ${prevLocation.y}px)`,
-                },
-                {
-                    transform: `translate(${Location.x}px, ${Location.y}px)`,
-                },
-            ],
-            {
-                duration: 2000,
-                easing: "ease-in-out",
-                direction: "normal",
-                fill: "forwards",
-            }
-        );
+        const newLocation = locationCalculater.getLocation(this.drawPosition);
+        const animation = this.animateElement(this.id, prevLocation, newLocation);
         await animation.finished;
-        await this.delay(1000)
-        this.applyCallBackOutAnimation()
-    }
+        await this.delay(1000);
+        this.applyCallBackOutAnimation();
+    }    
 
     setDrawPosition(drawPosition) {
         this.drawPosition = drawPosition;
     }
 }
+
