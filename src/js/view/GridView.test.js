9530d875426daf0a53bb7767d7f6bbb94ce1afee
minjeongHEO <hmjoung9770@gmail.com>
2024-04-03 13:19:18 +0900

    feat: gridView 단위 테스트 - isFirstIndex - isEven

--- src/js/view/GridView.test.js
+++ src/js/view/GridView.test.js
@@ -1,37 +1,119 @@
 import { isFirstIndex, isEven, isLastIndex, isToCopy } from './GridView.js';
 
-// given(테스트에 필요한 값 셋팅) -> when(실행) -> then(테스트)
-describe('gridView - Boolean 관련 테스트', function () {
-  '인덱스 값이 첫번째 인덱스 값일 때 true를 반환',
-    function () {
-      // *given
-      const index = 0;
-
-      // *when
-      const result = isFirstIndex(index);
-
-      // *then
-      expect(result).toBeTruthy();
-    };
-
-  test.each(['0', 1, undefined, null])('인덱스 값이 invalid한 값일 때 false를 반환', (input) => {
-    // given input
-
+describe('gridView - isFirstIndex 단위 테스트', () => {
+  test('첫번째 인덱스 값일 때 true를 반환', () => {
+    // given
+    const input = 0;
     // when
     const result = isFirstIndex(input);
+    // then
+    expect(result).toBe(true);
+  });
 
+  test('첫번째 인덱스 값이 아닐 때 false를 반환', () => {
+    // given
+    const input = 1;
+    // when
+    const result = isFirstIndex(input);
     // then
     expect(result).toBe(false);
   });
 
-  test("인덱스 값이 문자열 '0' 값일 때 false를 반환", function () {
-    // *given
-    const index = '0';
+  test('유효하지 않은 값(undefined)일 때 false를 반환', () => {
+    // given
+    const input = undefined;
+    // when
+    const result = isFirstIndex(input);
+    // then
+    expect(result).toBe(false);
+  });
 
-    // *when
-    const result = isFirstIndex(index);
+  test('유효하지 않은 값(null)일 때 false를 반환', () => {
+    // given
+    const input = null;
+    // when
+    const result = isFirstIndex(input);
+    // then
+    expect(result).toBe(false);
+  });
 
-    // *then
-    expect(undefined).toBeFalsy(); //tobe(false)
+  test("문자열 '0'일 때 false를 반환", () => {
+    // given
+    const input = '0';
+    // when
+    const result = isFirstIndex(input);
+    // then
+    expect(result).toBe(false);
+  });
+});
+
+describe('gridView - isEven 단위 테스트', () => {
+  test('짝수이면 true를 반환', () => {
+    // given
+    const input = 0;
+    // when
+    const result = isEven(input);
+    // then
+    expect(result).toBe(true);
+  });
+
+  test('짝수가 아니면 false를 반환', () => {
+    // given
+    const input = 1;
+    // when
+    const result = isEven(input);
+    // then
+    expect(result).toBe(false);
+  });
+
+  test('유효하지 않은 값(undefined)일 때 false를 반환', () => {
+    // given
+    const input = undefined;
+    // when
+    const result = isEven(input);
+    // then
+    expect(result).toBe(false);
+  });
+
+  test('유효하지 않은 값(null)일 때 false를 반환', () => {
+    // given
+    const input = null;
+    // when
+    const result = isEven(input);
+    // then
+    expect(result).toBe(false);
+  });
+
+  test("문자열 '0'일 때 false를 반환", () => {
+    // given
+    const input = '0';
+    // when
+    const result = isEven(input);
+    // then
+    expect(result).toBe(false);
+  });
+});
+
+describe('gridView - isLastIndex 단위 테스트', () => {
+  test('마지막 인덱스일 때 true를 반환', () => {
+    // given
+    const [index, maxLength] = [2, 3];
+
+    // when
+    const result = isLastIndex(index, maxLength);
+
+    // then
+    expect(result).toBe(true);
+  });
+
+  test('마지막 인덱스가 아닐 때 false를 반환', () => {
+    // given
+    const [index, maxLength] = [1, 3];
+
+    // when
+    const result = isLastIndex(index, maxLength);
+
+    // then
+    expect(result).toBe(false);
   });
 });
