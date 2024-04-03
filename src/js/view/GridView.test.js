d7cbc228fcf6d45dae926c7fee056b0e5675950d
minjeongHEO <hmjoung9770@gmail.com>
2024-04-03 14:04:38 +0900

    feat: gridView 단위 테스트 - isToCopy

--- src/js/view/GridView.test.js
+++ src/js/view/GridView.test.js
@@ -98,10 +98,8 @@ describe('gridView - isLastIndex 단위 테스트', () => {
   test('마지막 인덱스일 때 true를 반환', () => {
     // given
     const [index, maxLength] = [2, 3];
-
     // when
     const result = isLastIndex(index, maxLength);
-
     // then
     expect(result).toBe(true);
   });
@@ -109,10 +107,34 @@ describe('gridView - isLastIndex 단위 테스트', () => {
   test('마지막 인덱스가 아닐 때 false를 반환', () => {
     // given
     const [index, maxLength] = [1, 3];
-
     // when
     const result = isLastIndex(index, maxLength);
+    // then
+    expect(result).toBe(false);
+  });
+});
 
+describe('gridView - isToCopy 단위 테스트', () => {
+  test('첫번째 인덱스, 마지막 인덱스가 아니고 짝수일 때 true를 반환', () => {
+    // given
+    const [index, maxLength] = [2, 4];
+    // when
+    const result = isToCopy(index, maxLength);
+    // then
+    expect(result).toBe(true);
+  });
+
+  test.each([
+    ['index값이 undefined', undefined, 3],
+    ['maxLength값이 undefined', 1, undefined],
+    ['index값이 null', null, 3],
+    ['maxLength값이 null', 1, null],
+    ['index값이 string', '1', 3],
+    ['maxLength값이 string', 1, '3'],
+  ])('invalid한 값(%s)일 때 false를 반환', (description, index, maxLength) => {
+    // given input
+    // when
+    const result = isToCopy(index, maxLength);
     // then
     expect(result).toBe(false);
   });
