c81581c5f8c49d54e153ccf8e323328f33cfd4b9
임승현 <96limshyun@gmail.com>
2024-04-05 00:06:19 +0900

    [Feat] 루프 애니메이션 추가

--- src/js/view/components.js
+++ src/js/view/components.js
@@ -28,11 +28,19 @@ const appendTag = async(callBack, className, memory, fromQueue) => {
     memory.updateStatusByClassName(callBack, className, "push");
 };
 
+const runLoop = async() => {
+    const loopNode = document.querySelector(".animation__event_loop_icon")
+    loopNode.classList.add("spinAnimation")
+    await delay(1000)
+    loopNode.classList.remove("spinAnimation")
+
+}
+
 export const addQueueAnimation = async(className) => {
     const queueBoxes = [...document.querySelector(className).children]
     const queueName = className === ".animation__micro_task_box" ? "micro" : "macro";
     queueBoxes.forEach(curBox => curBox.classList.add("queue-move"))
-    await delay(1000)
+    await runLoop()
     queueBoxes.forEach(curBox => curBox.classList.remove("queue-move", queueName))
 }
 
