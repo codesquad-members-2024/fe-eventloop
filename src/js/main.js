ff97c064e3c27f8dd6176afd0f825c15926c9af4
정채영 <chaeyoung@jeongchaeyeong-ui-MacBookAir.local>
2024-04-05 10:44:28 +0900

    feat: 웹에서 코드 입력받기

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,8 @@
-import {
-  registerToWebAPI,
-  classifyIntoMacroAndMicro,
-  moveToQueue,
-  moveToCallstack,
-} from './controller.js';
-import { parseCode } from './parsing.js';
-import { code } from './test-code.js';
+import { getCode } from './input.js';
+// import { code } from './test-code.js';
 
 async function main() {
-  const tasks = parseCode(code);
-  console.log(tasks);
-  await registerToWebAPI(tasks);
-  const { microTask, macroTask } = classifyIntoMacroAndMicro(tasks);
-  await moveToQueue(microTask);
-  await moveToQueue(macroTask);
-  await moveToCallstack(microTask);
-  moveToCallstack(macroTask);
+  getCode();
 }
 
 main();
