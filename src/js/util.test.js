895daaf7656f4f5a33e918358b89c1f9dd4bc7d1
정채영 <chaeyoung@jeongchaeyeong-ui-MacBookAir.local>
2024-04-05 16:16:13 +0900

    chore: 테스트 설명 변경

--- src/js/util.test.js
+++ src/js/util.test.js
@@ -9,6 +9,6 @@ test('2000이 2가 되어야함', () => {
   expect(millisecondToSecond(2000)).toBe(2);
 });
 
-test('2000이 2가 되어야함', () => {
+test('배열의 첫번째 인덱스의 요소가 반환되어야 함', () => {
   expect(getFirstElementInArr([10, 1, 2, 3, 4, 5])).toBe(10);
 });
